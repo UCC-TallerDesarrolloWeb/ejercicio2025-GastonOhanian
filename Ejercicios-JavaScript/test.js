c0da627905b2c1e14eb881f3e2d57e297c622904
GastonOhanian <2412103@ucc.edu.ar>
2025-09-08 17:46:13 -0300

    Comenzamos con JS y jugamos para afianzar un poco con los contenidos

---
+++ Ejercicios-JavaScript/test.js
@@ -0,0 +1,27 @@
+let i=10;
+let h=100;
+
+
+let nombre='gasti';
+
+function Funcion(edad,peso) {
+  let persona;
+  persona=edad + peso;
+}
+
+const jugador=(deporte) =>{
+  let resultado;
+  switch (deporte) {
+    case 'futbol':
+      resultado= 'soy bueno';
+      break;
+    case 'rugby':
+      resultado='soy re bueno';
+      break;
+    default: resultado = 'somos todos re buenos';
+      break;
+}
+};
+// => es una forma de decir funcion}
+
+let saludo= nombre => 'Hola'+ nombre +'!!!';
