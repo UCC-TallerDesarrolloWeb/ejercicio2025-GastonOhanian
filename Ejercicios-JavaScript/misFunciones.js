a6491865db4ca8492edd3c5739b2403b3ec1b0bc
GastonOhanian <2412103@ucc.edu.ar>
2025-09-15 16:40:07 -0300

    Hicimos ejercicios en java y nos acomodamos mas con el trabajo entre ambos lenguajes

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -5,3 +5,51 @@
  * @param Parámetro B
  * @return Valor que retorna
  */
+
+const ConversorUnidades = (id, value )=>{
+  if (id==='metro'){
+
+        document.getElementById("pulgada").value=value*39,37;
+        document.getElementById("pie").value=value*3,281;
+        document.getElementById("yarda").value=value*1,1;
+}
+
+  if (id==='pie'){
+
+    document.getElementById("metro").value=value/3,281;
+    document.getElementById("pulgada").value=value*12;
+    document.getElementById("yarda").value=value/3;
+}
+
+if (id==="yarda"){
+
+    document.getElementById("metro").value=value/1.1;
+    document.getElementById("pie").value=value*12;
+    document.getElementById("pulgada").value=value/3;
+}
+
+if (id==="pulgada"){
+
+    document.getElementById("metro").value=value/39,37;
+    document.getElementById("pie").value=value/12
+    document.getElementById("yarda").value=value/36;
+}
+}
+
+
+
+const gradrad = (id, value)=>{
+  if (id==='grados'){
+
+        document.getElementById("unid_grados").value=(value*Math.pi/180).toFixed(4);
+}}
+
+
+
+const radgrad = (id, value)=>{
+  if (id==='unid_radianes'){
+
+        document.getElementById("radianes").value=(value * 180 / Math.PI).toFixed(2);
+}}
+
+
