8c132d53c00d72ee36dafc23c0dd88b5b53148ee
GastonOhanian <2412103@ucc.edu.ar>
2025-09-22 16:01:49 -0300

    Hicimos algunos ejercicios y terminamos de ver JS

--- Ejercicios-JavaScript/misFunciones.js
+++ Ejercicios-JavaScript/misFunciones.js
@@ -6,35 +6,6 @@
  * @return Valor que retorna
  */
 
-const ConversorUnidades = (id, value )=>{
-  if (id==='metro'){
-
-        document.getElementById("pulgada").value=value*39,37;
-        document.getElementById("pie").value=value*3,281;
-        document.getElementById("yarda").value=value*1,1;
-}
-
-  if (id==='pie'){
-
-    document.getElementById("metro").value=value/3,281;
-    document.getElementById("pulgada").value=value*12;
-    document.getElementById("yarda").value=value/3;
-}
-
-if (id==="yarda"){
-
-    document.getElementById("metro").value=value/1.1;
-    document.getElementById("pie").value=value*12;
-    document.getElementById("pulgada").value=value/3;
-}
-
-if (id==="pulgada"){
-
-    document.getElementById("metro").value=value/39,37;
-    document.getElementById("pie").value=value/12
-    document.getElementById("yarda").value=value/36;
-}
-}
 
 
 
@@ -52,4 +23,53 @@ const radgrad = (id, value)=>{
         document.getElementById("radianes").value=(value * 180 / Math.PI).toFixed(2);
 }}
 
+const convertirUnidades = (id,valor) => {
+    let met, pul, pie, yar;
 
+    if(valor.includes(",")){
+        valor = valor.replace(",", ".")
+    }
+    if(isNaN(valor)){
+        alert("El valor ingresado es incorrecto");
+        met = "";
+        pul = "";
+        pie = "";
+        yar = "";
+    } if(id==="metro"){
+        met = valor;
+        pul = valor*39.3701;
+        pie = valor*3.28084;
+        yar = valor*1.09361;
+    }else if(id==="pulgada"){
+        met = valor/39.3701;
+        pul = valor;
+        pie = valor/12;
+        yar = valor/36;
+    }
+    else if(id==="pie"){
+        met = valor/3,281;
+        pul = valor*12;
+        pie = valor;
+        yar = valor/3;
+    }else if(id==="yarda"){
+        met = valor/1.0936;
+        pul = valor*36;
+        pie = valor*3;
+        yar = valor;
+    }
+    document.lasUnidades.unid_metro.value = Math.round(met*100)/100;
+    document.lasUnidades.unid_pulgada.value = Math.round(pul*100)/100;
+    document.lasUnidades.unid_pie.value = Math.round(pie);
+    document.lasUnidades.unid_yarda.value = Math.round(yar);
+}
+
+
+
+
+function mostrar_ocultar(valorMO) {
+   if(valorMO === "val_mostrar"){
+    document.getElementById("divMO").style.display='block';
+   } else if (valorMO === "val_ocultar"){
+    document.getElementById ("divMO").style.display='none';
+   }
+}
